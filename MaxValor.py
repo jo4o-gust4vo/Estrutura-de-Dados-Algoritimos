ca711e62e677d14124c06ce2185b9aa6a66da086
jo4o-gust4vo <j.gustavo.devs@gmail.com>
2025-11-04 17:54:00 -0300

    Add função recursiva de valor max

---
+++ MaxValor.py
@@ -0,0 +1,27 @@
+def MaxValor(lista: list):
+    
+
+    if lista == []:
+        return 'lista vazia'
+    
+    if len(lista) == 1:
+        return lista[0]
+    
+    if len(lista) == 2:
+        return lista[0] if lista[0] > lista[1] else lista[1]
+    
+        
+    sub_max = MaxValor(lista[1:])
+    return lista[0] if lista[0] > sub_max else sub_max
+    
+
+    
+
+print(MaxValor([2,1]))
+
+minha_lista = [1,2,3]
+
+print(minha_lista[1:])
+
+    
+
